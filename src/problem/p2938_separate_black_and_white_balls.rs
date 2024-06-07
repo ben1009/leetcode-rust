87aae2d84a56dffbe8740b04166b611b8aa765d8
ben1009 <liuhe1009@gmail.com>
2024-06-07 16:27:23 +0800

    feat: 2938

---
+++ src/problem/p2938_separate_black_and_white_balls.rs
@@ -0,0 +1,78 @@
+/// [2938] Separate Black and White Balls
+///
+/// There are n balls on a table, each ball has a color black or white.
+/// You are given a 0-indexed binary string s of length n, where 1 and 0 represent black and white
+/// balls, respectively. In each step, you can choose two adjacent balls and swap them.
+/// Return the minimum number of steps to group all the black balls to the right and all the white
+/// balls to the left.  
+/// <strong class="example">Example 1:
+///
+/// Input: s = "101"
+/// Output: 1
+/// Explanation: We can group all the black balls to the right in the following way:
+/// - Swap s[0] and s[1], s = "011".
+/// Initially, 1s are not grouped together, requiring at least 1 step to group them to the right.
+/// <strong class="example">Example 2:
+///
+/// Input: s = "100"
+/// Output: 2
+/// Explanation: We can group all the black balls to the right in the following way:
+/// - Swap s[0] and s[1], s = "010".
+/// - Swap s[1] and s[2], s = "001".
+/// It can be proven that the minimum number of steps needed is 2.
+///
+/// <strong class="example">Example 3:
+///
+/// Input: s = "0111"
+/// Output: 0
+/// Explanation: All the black balls are already grouped to the right.
+///
+///  
+/// Constraints:
+///
+///     1 <= n == s.length <= 10^5
+///     s[i] is either '0' or '1'.
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/separate-black-and-white-balls/
+// discuss: https://leetcode.com/problems/separate-black-and-white-balls/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn minimum_steps(s: String) -> i64 {
+        if s.len() == 1 {
+            return 0;
+        }
+
+        let mut ret = 0;
+        let mut count = 0;
+        for b in s.as_bytes().iter().rev() {
+            if b == &b'0' {
+                count += 1;
+            } else {
+                ret += count;
+            }
+        }
+
+        ret
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2938() {
+        assert!(Solution::minimum_steps("101".to_string()) == 1);
+        assert!(Solution::minimum_steps("100".to_string()) == 2);
+        assert!(Solution::minimum_steps("0111".to_string()) == 0);
+        assert!(Solution::minimum_steps("1111".to_string()) == 0);
+        assert!(Solution::minimum_steps("0000".to_string()) == 0);
+        assert!(Solution::minimum_steps("0001".to_string()) == 0);
+        assert!(Solution::minimum_steps("1".to_string()) == 0);
+    }
+}
