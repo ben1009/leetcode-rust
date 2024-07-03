e4245aae58d00e1e9a42f0f2f738391464d4ce0c
ben1009 <liuhe1009@gmail.com>
2024-07-03 15:27:48 +0800

    feat: 560

---
+++ src/problem/p0560_subarray_sum_equals_k.rs
@@ -0,0 +1,60 @@
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum
+// equals to k.
+
+// A subarray is a contiguous non-empty sequence of elements within an array.
+
+// Example 1:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -1000 <= nums[i] <= 1000
+// -107 <= k <= 107
+
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/subarray-sum-equals-k/
+
+// submission codes start here
+
+impl Solution {
+    pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let mut ret = 0;
+        let mut sum = 0;
+        let mut dic = std::collections::HashMap::new();
+
+        for item in nums {
+            sum += item;
+            if sum == k {
+                ret += 1;
+            }
+            if let Some(v) = dic.get(&(sum - k)) {
+                ret += v;
+            }
+            *dic.entry(sum).or_insert(0) += 1;
+        }
+
+        ret
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_560() {
+        assert_eq!(Solution::subarray_sum(vec![1, 1, 1], 2), 2);
+        assert_eq!(Solution::subarray_sum(vec![1, 2, 3], 3), 2);
+        assert_eq!(Solution::subarray_sum(vec![1], 3), 0);
+    }
+}
